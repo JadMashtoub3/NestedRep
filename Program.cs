22022f190c31239bf0201f4e8039ae54fa675435
JadMashtoub3 <80442672+JadMashtoub3@users.noreply.github.com>
2021-04-28 12:04:54 +1000

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NestedRep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        string username;     
+        Console.WriteLine("Enter your name ");
+        username = Console.ReadLine();
+        
+        for (int i = 0; i < 10; i++)
+        Console.Write(username );
+        }
+    }
+}
